8fa4b63f4c4658182d296bc41fe39729715932ab
Cristian Pardo <cpardoca3@gmail.com>
2022-05-24 10:08:18 +0200

    Backend - Mock Data if the backend starts with no data

--- backend/src/app/model/mongo.client.js
+++ backend/src/app/model/mongo.client.js
@@ -14,8 +14,26 @@ const client = () =>{
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => {
+    .then(async () => {
         console.log("Connected to the database!");
+        const news = await db.news.find({})
+        if (news.length === 0) {
+            try {
+                await db.news.insertMany(
+                    [
+                        {title: "title",description: "description",content: "content",author:"author2",   status:"Archived",date: new Date(Date.now())},
+                        {title: "title2",description: "description2",content: "content2",author:"author1",status:"New",date: new Date(Date.now())},
+                        {title: "title3",description: "description3",content: "content3",author:"author2",status:"Archived",date: new Date(Date.now())},
+                        {title: "title4",description: "description4",content: "content4",author:"author1",status:"New",date: new Date(Date.now())},
+                        
+                    ]
+                );
+                console.log("Demo data created")
+            } catch (error) {
+                console.log("Cannot create the mock data", error);
+                process.exit();
+            }
+        }
     })
     .catch(err => {
         console.log("Cannot connect to the database!", err);
